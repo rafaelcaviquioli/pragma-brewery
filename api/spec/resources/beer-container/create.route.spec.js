a1a189d4b75496315419d1a879ddd58decca3f55
Fábio Elizandro <fabio@elizandro.com.br>
2017-09-17 20:33:14 -0300

    Cover all api with unit tests.

--- api/spec/resources/beer-container/create.route.spec.js
+++ api/spec/resources/beer-container/create.route.spec.js
@@ -1,16 +1,52 @@
 const createServer = require('../../../src/server/create-server');
+const Nes = require('nes');
 
 const method = 'POST';
-const url = '/beer-containers';
+const url = '/beer-container-temperature';
 
 describe(`${method} ${url}`, () => {
   it('returns "OK"', async () => {
     expect.assertions(2);
 
     const server = await createServer();
-    const response = await server.inject({ method, url });
+    const payload = { beer: 'Beer 1', temperature: 5 };
+    const response = await server.inject({ method, url, payload });
 
     expect(response.statusCode).toEqual(200);
-    expect(response.payload).toEqual('OK');
+    expect(JSON.parse(response.payload)).toEqual({ message: 'OK' });
+  });
+
+  it('validates payload', async () => {
+    expect.assertions(2);
+
+    const server = await createServer();
+    const payload = { temperature: 5 };
+    const response = await server.inject({ method, url, payload });
+
+    expect(response.statusCode).toEqual(400);
+    expect(JSON.parse(response.payload)).toMatchObject({ message: 'child \"beer\" fails because [\"beer\" is required]' });
+  });
+
+  it('broadcasts containers temperature to all clients', async (done) => {
+    const server = await createServer();
+    await server.start();
+    const client = new Nes.Client(`http://localhost:${server.info.port}`);
+
+    client.onUpdate = async message => {
+      client.disconnect();
+      await server.stop();
+
+      expect(message).toEqual({
+        payload: { beer: 'Beer 1', temperature: 5 },
+        type: 'BEER_CONTAINER_TEMPERATURE_CREATED'
+      });
+
+      done();
+    };
+
+    client.connect(async () => {
+      const payload = { beer: 'Beer 1', temperature: 5 };
+      await server.inject({ method, url, payload });
+    });
   });
 });
---
+++ api/spec/resources/beer-container/payload.schema.spec.js
@@ -0,0 +1,37 @@
+const Joi = require('joi');
+const payloadSchema = require('../../../src/resources/beer-container-temperature/payload.schema');
+
+describe('#payloadSchema', () => {
+  describe('#beer', () => {
+    const schema = payloadSchema.beer;
+
+    it('is string', () => {
+      const { error: { details } } = Joi.validate(1, schema);
+      expect(details).toMatchObject([{ message: '"value" must be a string' }]);
+    });
+
+    it('is required', () => {
+      const { error: { details } } = Joi.validate(undefined, schema);
+      expect(details).toMatchObject([{ message: '"value" is required' }]);
+    });
+  });
+
+  describe('#temperature', () => {
+    const schema = payloadSchema.temperature;
+
+    it('is number', () => {
+      const { error: { details } } = Joi.validate('', schema);
+      expect(details).toMatchObject([{ message: '"value" must be a number' }]);
+    });
+
+    it('is integer', () => {
+      const { error: { details } } = Joi.validate(1.1, schema);
+      expect(details).toMatchObject([{ message: '"value" must be an integer' }]);
+    });
+
+    it('is required', () => {
+      const { error: { details } } = Joi.validate(undefined, schema);
+      expect(details).toMatchObject([{ message: '"value" is required' }]);
+    });
+  });
+});
